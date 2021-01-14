b715e938a0b82279d1ac3817cd6bf8bfa361738a
Max <addergtx@gmail.com>
2021-01-14 12:36:20 +0300

    Группирую только по дате

--- src/MSSQLServerMonitoring.WebApp/Pages/Alerts/Alerts.cshtml.cs
+++ src/MSSQLServerMonitoring.WebApp/Pages/Alerts/Alerts.cshtml.cs
@@ -18,46 +18,22 @@ namespace MSSQLServerMonitoring.WebApp.Pages.Alerts
         public List<Alert> Alerts { get; set; }
         public string IdSort { get; set; }
         public string DateSort { get; set; }
-        public string MessageSort { get; set; }
-        public string StatusSort { get; set; }
 
         public void OnGet(string sortOrder)
         {
-            List<Alert> alerts = Alerts;
+            List<Alert> alerts = _repositoryWrapper.Alert.GetAll().Result;
 
-            IdSort = String.IsNullOrEmpty(sortOrder) ? "id_desc" : "";
-            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-            MessageSort = sortOrder == "Message" ? "message_desc" : "Message";
-            StatusSort = sortOrder == "Status" ? "status_desc" : "Status";
-
+            DateSort = String.IsNullOrEmpty(sortOrder) ? "date_desc" : "";
             IEnumerable<Alert> alertsGroup = from ag in alerts
                                              select ag;
 
             switch (sortOrder)
             {
-                case "id_desc":
-                    alertsGroup = alertsGroup.OrderByDescending(ag => ag.Id);
-                    break;
-                case "Date":
-                    alertsGroup = alertsGroup.OrderBy(ag => ag.TimeStamp);
-                    break;
                 case "date_desc":
                     alertsGroup = alertsGroup.OrderByDescending(ag => ag.TimeStamp);
                     break;
-                case "Massage":
-                    alertsGroup = alertsGroup.OrderBy(ag => ag.Message);
-                    break;
-                case "message_desc":
-                    alertsGroup = alertsGroup.OrderByDescending(ag => ag.Message);
-                    break;
-                case "Status":
-                    alertsGroup = alertsGroup.OrderBy(ag => ag.Status);
-                    break;
-                case "status_desc":
-                    alertsGroup = alertsGroup.OrderByDescending(ag => ag.Status);
-                    break;
                 default:
-                    alertsGroup = alertsGroup.OrderBy(ag => ag.Id);
+                    alertsGroup = alertsGroup.OrderBy(ag => ag.TimeStamp);
                     break;
             }
 
